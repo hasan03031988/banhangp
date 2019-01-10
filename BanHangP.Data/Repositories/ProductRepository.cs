c1445636bc4af4bbaf44a1de981b265ce1d3df8a
HaSan <hoangsan0303@gmail.com>
2019-01-10 23:17:39 +0900

    commit migration and repository

---
+++ BanHangP.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using BanHangP.Data.Infrastructure;
+using BanHangP.Model.Models;
+
+namespace BanHangP.Data.Repositories
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+    }
+
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
